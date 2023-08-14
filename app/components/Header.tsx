a3d49e206d738c699d803a308c183dffe2b9b925
YouJin Cho <hycoho1996@gmail.com>
2023-08-15 01:06:15 +0900

    ✨ feat : Implement header component and design

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,59 @@
 "use client"
 
+import { Button, Grid, TextField, styled } from "@mui/material"
 import React from "react"
+import { black200, black500, shadow, white } from "../styles/theme"
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
+import SearchSharpIcon from '@mui/icons-material/SearchSharp'
+
+const HeaderContainer = styled(Grid)({
+  display: 'flex',
+  padding: '10px 50px',
+  alignItems: 'center',
+  boxShadow: `${shadow}`,
+  backgroundColor: `${white}`,
+})
+
+const SearchField = styled(TextField)({
+  width: '500px',
+  height: '40px',
+  '.MuiOutlinedInput-root': {
+    borderRadius: '15px',
+  },
+  '.MuiInputBase-input': {
+    padding: '10px 20px',
+  }
+})
+
+const MyPageButton = styled(Button)({
+  textTransform: 'none',
+  padding: '10px 0px 10px 12px',
+  borderRadius: '15px',
+  color: `${black500}`,
+})
 
 const Header: React.FC = () => {
-  
   return (
-    <div>
-    Hello
-    </div>
+    <HeaderContainer container justifyContent="space-between">
+      <Grid item>
+        <img src="./logo.png" alt="logo" style={{ cursor: 'pointer', position: 'relative', top: '3px' }} />
+      </Grid>
+      <Grid item>
+        <SearchField
+          variant="outlined"
+          placeholder="Search"
+          InputProps={{
+            endAdornment: <SearchSharpIcon style={{ cursor: 'pointer' }}/>
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <MyPageButton 
+          variant="outlined"
+          style={{ borderColor: `${black200}` }}
+          startIcon={<PersonOutlineOutlinedIcon style={{ cursor: 'pointer' }}/>} />
+      </Grid>
+    </HeaderContainer>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 "use client"
 
 import styles from "./page.module.css";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
     </main>
   );
 }
