86e4ac0809ddc0dfc740179ef3974ebb4a404691
YouJin Cho <hycoho1996@gmail.com>
2023-08-21 16:29:40 +0900

    ✨ feat : Main page component and design implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 "use client"
 
-import styles from "./page.module.css";
+import Login from "./login/page";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-    </main>
-  );
+    <Login />
+  )
 }
