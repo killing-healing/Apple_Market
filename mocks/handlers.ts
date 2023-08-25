73f164e238fa67feee5f397ce931fd23110da8e9
bidanee <dleksql119@gmail.com>
2023-08-25 17:50:48 +0900

    📝 chore : msw handler

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,7 +1,36 @@
 import { rest } from "msw";
+import { Chatting, Login, Register, Room, RoomDetail } from "./types";
+import { chatUsers, users } from "./data/testData";
+
+const roomId = "1";
 
 export const handlers = [
-  rest.post("/register", (_req, res, ctx) => {
+  rest.post("/register", (req, res, ctx) => {
+    const { email, password, nickname, gender } = <Register>req.body;
+
+    const checkEmail = users.find((user) => user.email === email);
+    const checkNickname = users.find((user) => user.nickname === nickname);
+    if (!!checkEmail) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          code: "Fail",
+          data: {
+            msg: "이미 있는 이메일 입니다.",
+          },
+        })
+      );
+    } else if (!!checkNickname) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          code: "Fail",
+          data: {
+            msg: "이미 있는 닉네임 입니다.",
+          },
+        })
+      );
+    }
     return res(
       ctx.status(200),
       ctx.json({
@@ -15,15 +44,25 @@ export const handlers = [
     );
   }),
 
-  rest.post("/login", async (_req, res, ctx) => {
+  rest.post("/login", async (req, res, ctx) => {
+    const { email, password } = <Register>req.body;
+    const isUser = users.find((user) => user.email === email);
+    if (!isUser) {
+      return res(ctx.status(401));
+    }
     return res(
       ctx.status(200),
-      ctx.json({
+      ctx.delay(1000),
+      ctx.json<Login>({
         code: "SUCCESS",
-        status: "OK",
-        msg: "성공",
+        msg: "로그인 성공",
+        status: "200",
         data: {
           "ACCESS-TOKEN": "123QWEasdZXC",
+          email: isUser.email,
+          password: isUser.password,
+          nickname: isUser.nickname,
+          gender: isUser.gender,
         },
       })
     );
@@ -41,7 +80,8 @@ export const handlers = [
     );
   }),
 
-  rest.post("/rooms", (_req, res, ctx) => {
+  rest.post("/rooms", (req, res, ctx) => {
+    const { name, email } = <Room>req.body;
     return res(
       ctx.status(200),
       ctx.json({
@@ -50,7 +90,7 @@ export const handlers = [
         msg: "Room created successfully",
         data: [
           {
-            userName: "apple",
+            name: "apple",
             roomId: "1",
             createdAt: "2023-08-15",
             msg: "룸 생성 완료",
@@ -60,7 +100,19 @@ export const handlers = [
     );
   }),
 
-  rest.post("/rooms/:roomId/join", (_req, res, ctx) => {
+  rest.post(`/rooms/${roomId}/join`, (req, res, ctx) => {
+    const { email } = <Room>req.body;
+    const accessUser = chatUsers.find((user) => user.email === email);
+    if (!accessUser) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          code: "FAIL",
+          status: 400,
+          msg: "접근권한이 없습니다.",
+        })
+      );
+    }
     return res(
       ctx.status(200),
       ctx.json({
@@ -73,7 +125,18 @@ export const handlers = [
       })
     );
   }),
-  
+
+  rest.put(`/rooms/${roomId}/leave`, (req, res, ctx) => {
+    const { email } = <Register>req.body;
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: "SUCCESS",
+        msg: "채팅방을 나갑니다.",
+      })
+    );
+  }),
+
   rest.get("/rooms", (_req, res, ctx) => {
     return res(
       ctx.status(200),
@@ -84,24 +147,58 @@ export const handlers = [
         data: [
           {
             roomId: "2",
-            userName: "bpple",
+            name: "bpple",
             createAt: "2023-08-13",
           },
           {
             roomId: "3",
-            userName: "cpple",
+            name: "cpple",
             createAt: "2023-08-12",
           },
           {
             roomId: "4",
-            userName: "dpple",
+            name: "dpple",
             createAt: "2023-07-13",
           },
           {
             roomId: "5",
-            userName: "epple",
+            name: "epple",
             createAt: "2023-06-13",
           },
+        ],
+      })
+    );
+  }),
+
+  rest.get(`/rooms/${roomId}`, (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json<RoomDetail>({
+        room: { roomId: "1", name: "apple" },
+        users: ["one", "two"],
+      })
+    );
+  }),
+
+  rest.post(`/rooms/${roomId}/message`, (req, res, ctx) => {
+    const { email, msg } = <Chatting>req.body;
+    return res(
+      ctx.status(200),
+      ctx.json({
+        msg: "Message sent successfully",
+      })
+    );
+  }),
+
+  rest.get(`/rooms/${roomId}/message`, (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        msg: [
+          { nickname: "one", msg: "들어오시죠", timestamp: "시간1" },
+          { nickname: "two", msg: "싫어요", timestamp: "시간2" },
+          { nickname: "one", msg: "....", timestamp: "시간3" },
+          { nickname: "two", msg: "오분만 시간 주세요", timestamp: "시간4" },
         ],
       })
     );
